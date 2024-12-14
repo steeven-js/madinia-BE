d0ad95ca545d5a6982d1399dd89506011bd0d357
steeven-js <jacques.steeven@gmail.com>
2024-12-14 00:38:16 -0400

    feat: Ajouter la configuration de Tailwind CSS et des composants Blade pour le frontend

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmailTestController;
-use App\Http\Controllers\StripeCheckoutController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmailTestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +16,15 @@ Route::get('/payment/success', function (Request $request) {
         'session_id' => $request->session_id
     ]);
 })->name('payment.success');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
